b0ddce985149c67fb61482614c7e565e9ef44cb7
Lex Zavala <lex.zavala7@gmail.com>
2021-05-05 12:58:35 -0500

    added functionality to code, still not working

--- js/script.js
+++ js/script.js
@@ -1,5 +1,51 @@
 "use strict"
+var mySeconds;
+var intervalHandle;
 
-function tick(){
-    var timeDisplay = document.
+function resetPage(){
+    // document.getElementsByClassName('inputField');
 }
+function tick(){
+    var timeDisplay = document.getElementById('time');
+    var min = Math.floor(mySeconds/60);
+    var sec = mySeconds - (min*60);
+    if (sec < 10) {
+        sec="0"+sec;
+    }
+
+    var message = min.toString() + ":" + sec;
+    timeDisplay.innerHTML = message;
+
+    if (mySeconds === 0){
+        // timeDisplay.innerHTML = "DONE";
+        alert("Time is up");
+        clearInterval(intervalHandle);
+        resetPage();
+    }
+    mySeconds--;
+}
+
+function startCounter(){
+    var myInput = document.getElementsByClassName('inputField').value;
+    if (isNaN(parseFloat(myInput.toString()))){
+        alert("Type a valid number please");
+        return;
+    }
+    mySeconds=myInput*60;
+
+    intervalHandle=setInterval(tick, 1000);
+
+    document.getElementsByClassName('inputField').style.display="none";
+}
+
+$('#myButton').click(function (){
+    startCounter();
+});
+
+// var input = document.getElementsByClassName('inputField');
+// var checkEnter = input.addEventListener('keydown', function (e){
+//     if (e.code.includes("Enter")){
+//         startCounter();
+//     }
+// });
+
